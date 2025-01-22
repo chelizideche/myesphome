fb855f39507f6826afc65fd1e089c5357d6a785c
Djordje <6750655+DjordjeMandic@users.noreply.github.com>
2025-01-22 02:59:22 +0100

    Add PHASES mapping to Python codegen for EnergySampler

--- esphome/components/energy_sampler/__init__.py
+++ esphome/components/energy_sampler/__init__.py
@@ -6,3 +6,9 @@ EnergySampler = energy_sampler_ns.class_("EnergySampler")
 Phase = energy_sampler_ns.enum("Phase", is_class=True)
 PhaseSampleData = energy_sampler_ns.struct("PhaseSampleData")
 EnergySampleData = energy_sampler_ns.struct("EnergySampleData")
+
+PHASES = {
+    "PHASE_1": Phase.PHASE_1,
+    "PHASE_2": Phase.PHASE_2,
+    "PHASE_3": Phase.PHASE_3
+}
