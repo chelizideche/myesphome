354e37b39d4440f3cce2365ea96adc071ce3b3a4
clydebarrow <2366188+clydebarrow@users.noreply.github.com>
2025-02-06 15:58:27 +1100

    Implement packet transport for uart platform

---
+++ esphome/components/uart/packet_transport/__init__.py
@@ -0,0 +1,19 @@
+from esphome.components.packet_transport import (
+    PacketTransport,
+    new_packet_transport,
+    transport_schema,
+)
+from esphome.cpp_types import PollingComponent
+
+from .. import UART_DEVICE_SCHEMA, register_uart_device, uart_ns
+
+CODEOWNERS = ["@clydebarrow"]
+
+UDPTransport = uart_ns.class_("UARTTransport", PacketTransport, PollingComponent)
+
+CONFIG_SCHEMA = transport_schema(UDPTransport).extend(UART_DEVICE_SCHEMA)
+
+
+async def to_code(config):
+    var = await new_packet_transport(config)
+    await register_uart_device(var, config)
