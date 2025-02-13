06a32e26decb0f1f03a61c453017acdabb0c5203
Jordan Zucker <jordan.zucker@gmail.com>
2025-02-13 14:21:42 -0800

    Last pass for now

--- esphome/components/prometheus/prometheus_handler.cpp
+++ esphome/components/prometheus/prometheus_handler.cpp
@@ -885,7 +885,9 @@ void PrometheusHandler::climate_row_(AsyncResponseStream *stream, climate::Clima
   // Now see if position is supported
   if (obj->get_traits().get_supports_current_temperature()) {
     std::string current_temp = "current_temperature";
-    climate_mode_row_(stream, obj, area, node, friendly_name, current_temp, obj->current_temperature)
+    int8_t current_accuracy = traits.get_current_temperature_accuracy_decimals();
+    climate_mode_row_(stream, obj, area, node, friendly_name, current_temp,
+                      value_accuracy_to_string(obj->current_temperature, current_accuracy))
   }
 }
 #endif
