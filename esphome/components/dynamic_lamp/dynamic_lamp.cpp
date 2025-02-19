a3a2eff663c6d993f5a2f98e33fb0c038bc4ab97
Oliver Kleinecke <kleinecke.oliver@googlemail.com>
2025-02-19 13:02:11 +0100

    fix: streamline read_timers_to_log function by removing unnecessary loop and improving log message format

--- esphome/components/dynamic_lamp/dynamic_lamp.cpp
+++ esphome/components/dynamic_lamp/dynamic_lamp.cpp
@@ -302,19 +302,14 @@ std::vector<bool> DynamicLampComponent::build_lamp_list_from_list_str_(std::stri
 }
 
 void DynamicLampComponent::read_timers_to_log() {
-  uint8_t i = 0;
-  for (i = 0; i < 16; i++) {
-    if (this->active_lamps_[i].active) {
-      DynamicLampTimer timer;
-      this->fram_->read((2048), reinterpret_cast<unsigned char *>(&timer), 24);
-      for (uint8_t j = 0; j < 16; j++) {
-        bool lamp_included = static_cast<bool>(timer.lamp_list[j / 8] & (1 << (j % 8)));
-        if (lamp_included && this->active_lamps_[j].active) {
-          ESP_LOGV(TAG, "Timer found for lamp %s |: active: %d, action: %d, hour: %d, minute: %d, monday: %d, tuesday: %d, wednesday: %d, thursday: %d, friday: %d, saturday: %d, sunday: %d",
-                   this->active_lamps_[j].name, timer.active, timer.action, timer.hour, timer.minute, timer.monday, timer.tuesday,
-                   timer.wednesday, timer.thursday, timer.friday, timer.saturday, timer.sunday);
-        }
-      }
+  DynamicLampTimer timer;
+  this->fram_->read((2048), reinterpret_cast<unsigned char *>(&timer), 24);
+  for (uint8_t j = 0; j < 16; j++) {
+    bool lamp_included = static_cast<bool>(timer.lamp_list[j / 8] & (1 << (j % 8)));
+    if (lamp_included && this->active_lamps_[j].active) {
+      ESP_LOGV(TAG, "Timer found for lamp %s [ active: %d, action: %d, hour: %d, minute: %d, monday: %d, tuesday: %d, wednesday: %d, thursday: %d, friday: %d, saturday: %d, sunday: %d ]",
+               this->active_lamps_[j].name, timer.active, timer.action, timer.hour, timer.minute, timer.monday, timer.tuesday,
+               timer.wednesday, timer.thursday, timer.friday, timer.saturday, timer.sunday);
     }
   }
 }
