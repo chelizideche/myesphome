cd28114b6c2736761bb5c1b3cfbb9f8f314a131d
Martin Eckardt <martin@eckardt.name>
2025-03-07 19:28:04 +0100

    changed newlines to LF

--- esphome/components/mcp4151/output.py
+++ esphome/components/mcp4151/output.py
@@ -12,9 +12,10 @@ CONFIG_SCHEMA = cv.Schema({
     cv.GenerateID(): cv.declare_id(MCP4151),
 }).extend(cv.COMPONENT_SCHEMA).extend(spi.spi_device_schema())
 
+
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
-    
+
     await spi.register_spi_device(var, config)
     await cg.register_component(var, config)
     await output.register_output(var, config)
