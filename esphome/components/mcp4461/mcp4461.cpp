eba4d66626596208729e3cee2ae3625db00cd030
Oliver Kleinecke <okleinecke@web.de>
2025-03-26 11:34:36 +0100

    Update esphome/components/mcp4461/mcp4461.cpp
    Co-authored-by: Keith Burzinski <kbx81x@gmail.com>

--- esphome/components/mcp4461/mcp4461.cpp
+++ esphome/components/mcp4461/mcp4461.cpp
@@ -110,12 +110,7 @@ void Mcp4461Component::loop() {
     // is prohibited in public methods
     if (this->reg_[i].update_terminal) {
       // set terminal register changes
-      Mcp4461TerminalIdx terminal_connector;
-      if (i < 2) {
-        terminal_connector = Mcp4461TerminalIdx::MCP4461_TERMINAL_0;
-      } else {
-        terminal_connector = Mcp4461TerminalIdx::MCP4461_TERMINAL_1;
-      }
+      Mcp4461TerminalIdx terminal_connector =  i < 2 ? Mcp4461TerminalIdx::MCP4461_TERMINAL_0 : Mcp4461TerminalIdx::MCP4461_TERMINAL_1;
       uint8_t new_terminal_value = this->calc_terminal_connector_byte_(terminal_connector);
       ESP_LOGV(TAG, "updating terminal %" PRIu8 " to new value %" PRIu8, static_cast<uint8_t>(terminal_connector),
                new_terminal_value);
