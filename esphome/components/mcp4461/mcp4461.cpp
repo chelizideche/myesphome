1ab147dbe3a84290d8b34c687e0601b9a91a0dde
Oliver Kleinecke <okleinecke@web.de>
2025-03-26 11:42:30 +0100

    Update esphome/components/mcp4461/mcp4461.cpp
    Co-authored-by: Keith Burzinski <kbx81x@gmail.com>

--- esphome/components/mcp4461/mcp4461.cpp
+++ esphome/components/mcp4461/mcp4461.cpp
@@ -384,12 +384,7 @@ bool Mcp4461Component::decrease_wiper_(Mcp4461WiperIdx wiper) {
 }
 
 uint8_t Mcp4461Component::calc_terminal_connector_byte_(Mcp4461TerminalIdx terminal_connector) {
-  uint8_t i;
-  if ((static_cast<uint8_t>(terminal_connector) == 0 || static_cast<uint8_t>(terminal_connector) == 1)) {
-    i = 0;
-  } else {
-    i = 2;
-  }
+  uint8_t i = static_cast<uint8_t>(terminal_connector) <= 1 ? 0 : 2;
   uint8_t new_value_byte = 0;
   new_value_byte += static_cast<uint8_t>(this->reg_[(i + 1)].terminal_b);
   new_value_byte += static_cast<uint8_t>(this->reg_[(i + 1)].terminal_w) << 1;
