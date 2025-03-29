60e6c59709eedb3c572db65cfbd2c4a16bfada36
Jonathan Swoboda <154711427+swoboda1337@users.noreply.github.com>
2025-03-28 21:44:37 -0400

    Add comment

--- esphome/components/i2c/i2c_bus_esp_idf.cpp
+++ esphome/components/i2c/i2c_bus_esp_idf.cpp
@@ -182,9 +182,9 @@ ErrorCode IDFI2CBus::readv(uint8_t address, ReadBuffer *buffers, size_t cnt) {
     const auto &buf = buffers[i];
     if (buf.len == 0)
       continue;
+    // the last byte of read before stop should always be nack,
+    // split the last read len is larger than 1
     if (i == cnt - 1) {
-      // the last byte of read before stop should always be nack,
-      // split the last read if required
       if (buf.len > 1) {
         i2c_operation_job_t data{};
         data.command = I2C_MASTER_CMD_READ;
