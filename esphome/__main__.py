esphome/esphome

commit ac0549578197ae4c6c7a83a53e1b17a82bcfdb61

Dont do mqtt ip lookup if `use_address` has ip address (#5096)
* Dont do mqtt ip lookup id `use_address` is in config

* Fix after actually testing =)

esphome/__main__.py
@@ -32,7 +32,7 @@ from esphome.const import (
     SECRETS_FILES,
 )
 from esphome.core import CORE, EsphomeError, coroutine
-from esphome.helpers import indent
+from esphome.helpers import indent, is_ip_address
 from esphome.util import (
     run_external_command,
     run_external_process,
@@ -308,8 +308,10 @@ def upload_program(config, args, host):
     password = ota_conf.get(CONF_PASSWORD, "")
 
     if (
-        get_port_type(host) == "MQTT" or config[CONF_MDNS][CONF_DISABLED]
-    ) and CONF_MQTT in config:
+        not is_ip_address(CORE.address)
+        and (get_port_type(host) == "MQTT" or config[CONF_MDNS][CONF_DISABLED])
+        and CONF_MQTT in config
+    ):
         from esphome import mqtt
 
         host = mqtt.get_esphome_device_ip(

chelizideche/myesphome

commit 662df35fe9241e104e0f17b2115ef156919e13e7

Fix '--device MQTT' for devices with static IP
An earlier commit disabled the MQTT IP lookup if the device has a static
IP address in CORE.address. That made sense for the automatic fallback
to MQTT when mDNS is disabled, but not if the user explicitly asked for
MQTT.

Rework the condition to apply that check only to the !mDNS case, and clean
it up to be a little more understandable.

Fixes: ac0549578197 ("Dont do mqtt ip lookup if `use_address` has ip address (#5096)")

esphome/__main__.py
@@ -375,10 +375,12 @@ def upload_program(config, args, host):
     password = ota_conf.get(CONF_PASSWORD, "")
 
     if (
-        not is_ip_address(CORE.address)  # pylint: disable=too-many-boolean-expressions
-        and (get_port_type(host) == "MQTT" or config[CONF_MDNS][CONF_DISABLED])
-        and CONF_MQTT in config
+        CONF_MQTT in config  # pylint: disable=too-many-boolean-expressions
         and (not args.device or args.device in ("MQTT", "OTA"))
+        and (
+            ((config[CONF_MDNS][CONF_DISABLED]) and not is_ip_address(CORE.address))
+            or get_port_type(host) == "MQTT"
+        )
     ):
         from esphome import mqtt
 