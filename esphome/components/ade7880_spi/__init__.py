cyberhuman/esphome

[ade7880] add support for ade7880_spi

esphome/components/ade7880_spi/__init__.py
@@ -1,0 +1,1 @@
+CODEOWNERS = ["@kpfleming"]

chelizideche/myesphome

esphome/components/ade7880_spi/__init__.py
@@ -1,0 +1,1 @@
+CODEOWNERS = ["@cyberhuman"]