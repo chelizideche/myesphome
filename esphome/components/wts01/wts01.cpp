779aaac75088036282cce26c4e9a8f94a3e2aa01
Antoine Lépée <antoine@lepee.me>
2025-04-14 12:36:01 +0200

    refactor(wts01): Use 'this' for protected member var

--- esphome/components/wts01/wts01.cpp
+++ esphome/components/wts01/wts01.cpp
@@ -40,34 +40,34 @@ void WTS01Sensor::dump_config() {
 
 void WTS01Sensor::handle_char_(uint8_t c) {
   // Reset buffer if we're at the start of a new packet
-  if (c == HEADER_1 && buffer_pos_ == 0) {
-    buffer_[buffer_pos_++] = c;
+  if (c == HEADER_1 && this->buffer_pos_ == 0) {
+    this->buffer_[this->buffer_pos_++] = c;
     return;
   }
 
   // Check header sequence
-  if (buffer_pos_ == 1 && c != HEADER_2) {
-    buffer_pos_ = 0;
+  if (this->buffer_pos_ == 1 && c != HEADER_2) {
+    this->buffer_pos_ = 0;
     return;
   }
 
-  if (buffer_pos_ == 2 && c != HEADER_3) {
-    buffer_pos_ = 0;
+  if (this->buffer_pos_ == 2 && c != HEADER_3) {
+    this->buffer_pos_ = 0;
     return;
   }
 
-  if (buffer_pos_ == 3 && c != HEADER_4) {
-    buffer_pos_ = 0;
+  if (this->buffer_pos_ == 3 && c != HEADER_4) {
+    this->buffer_pos_ = 0;
     return;
   }
 
   // Add byte to buffer
-  buffer_[buffer_pos_++] = c;
+  this->buffer_[this->buffer_pos_++] = c;
 
   // Process complete packet
-  if (buffer_pos_ == PACKET_SIZE) {
-    process_packet_();
-    buffer_pos_ = 0;
+  if (this->buffer_pos_ == PACKET_SIZE) {
+    this->process_packet_();
+    this->buffer_pos_ = 0;
   }
 }
 
@@ -77,9 +77,10 @@ void WTS01Sensor::process_packet_() {
   // header            T  Td Ck  - T = Temperature, Td = Temperature decimal, Ck = Checksum
 
   // Check if packet is valid (we could add checksum verification here if needed)
-  if (buffer_[0] == HEADER_1 && buffer_[1] == HEADER_2 && buffer_[2] == HEADER_3 && buffer_[3] == HEADER_4) {
+  if (this->buffer_[0] == HEADER_1 && this->buffer_[1] == HEADER_2 && this->buffer_[2] == HEADER_3 &&
+      this->buffer_[3] == HEADER_4) {
     // Extract temperature value
-    uint8_t temp = buffer_[6];
+    uint8_t temp = this->buffer_[6];
     int sign = 1;
 
     // Handle negative temperatures
@@ -89,7 +90,7 @@ void WTS01Sensor::process_packet_() {
     }
 
     // Calculate temperature (temp + decimal/100)
-    float temperature = sign * (static_cast<float>(temp) + (static_cast<float>(buffer_[7]) / 100.0f));
+    float temperature = sign * (static_cast<float>(temp) + (static_cast<float>(this->buffer_[7]) / 100.0f));
 
     // Store the temperature value but don't publish yet - wait for update() to be called
     this->current_temperature_ = temperature;
