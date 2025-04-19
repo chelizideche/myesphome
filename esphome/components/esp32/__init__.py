4a829a23dc34e1cf1a2307815f4214aeb44ab535
Jonathan Swoboda <154711427+swoboda1337@users.noreply.github.com>
2025-04-18 22:45:18 -0400

    Use 3.0.7 as default

--- esphome/components/esp32/__init__.py
+++ esphome/components/esp32/__init__.py
@@ -244,11 +244,11 @@ def _format_framework_espidf_version(
 # The default/recommended arduino framework version
 #  - https://github.com/espressif/arduino-esp32/releases
 #  - https://api.registry.platformio.org/v3/packages/platformio/tool/framework-arduinoespressif32
-RECOMMENDED_ARDUINO_FRAMEWORK_VERSION = cv.Version(2, 0, 5)
+RECOMMENDED_ARDUINO_FRAMEWORK_VERSION = cv.Version(3, 0, 7)
 # The platformio/espressif32 version to use for arduino frameworks
 #  - https://github.com/platformio/platform-espressif32/releases
 #  - https://api.registry.platformio.org/v3/packages/platformio/platform/espressif32
-ARDUINO_PLATFORM_VERSION = cv.Version(5, 4, 0)
+ARDUINO_PLATFORM_VERSION = cv.Version(51, 3, 7)
 
 # The default/recommended esp-idf framework version
 #  - https://github.com/espressif/esp-idf/releases
