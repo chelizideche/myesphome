57dca090c16a0e86bde60dfafd3feb4039fa0650
Jesse Hills <3060199+jesserockz@users.noreply.github.com>
2025-05-01 14:00:04 +1200

    Update esphome/components/pca9685/pca9685_output.cpp
    Co-authored-by: Clyde Stubbs <2366188+clydebarrow@users.noreply.github.com>

--- esphome/components/pca9685/pca9685_output.cpp
+++ esphome/components/pca9685/pca9685_output.cpp
@@ -103,7 +103,7 @@ void PCA9685Output::loop() {
   const uint16_t num_channels = this->max_channel_ - this->min_channel_ + 1;
   const uint16_t phase_delta_begin = 4096 / num_channels;
   for (uint8_t channel = this->min_channel_; channel <= this->max_channel_; channel++) {
-    uint16_t phase_begin = uint16_t(channel - this->min_channel_) * phase_delta_begin;
+    uint16_t phase_begin = (channel - this->min_channel_) * phase_delta_begin;
     uint16_t phase_end;
     uint16_t amount = this->pwm_amounts_[channel];
     if (amount == 0) {
