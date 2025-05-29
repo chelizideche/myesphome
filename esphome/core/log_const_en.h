352cc67da4ea3643c4925cda9ed5b3d70280f254
Keith Burzinski <kbx81x@gmail.com>
2025-05-28 20:15:39 -0500

    Use `constexpr`

--- esphome/core/log_const_en.h
+++ esphome/core/log_const_en.h
@@ -1,4 +1,4 @@
 #pragma once
 
-#define ESP_LOG_MSG_COMM_FAIL "Communication failed"
-#define ESP_LOG_MSG_COMM_FAIL_FOR "Communication failed for '%s'"
+constexpr const char *const ESP_LOG_MSG_COMM_FAIL = "Communication failed";
+constexpr const char *const ESP_LOG_MSG_COMM_FAIL_FOR = "Communication failed for '%s'";
