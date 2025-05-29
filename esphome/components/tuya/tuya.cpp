ac3bd3ff13945abb4d65da71bc3b4abd03211f7c
SaschaKP <k.puligheddu@alice.it>
2025-05-29 16:38:29 +0200

    fix for tests by CI

--- esphome/components/tuya/tuya.cpp
+++ esphome/components/tuya/tuya.cpp
@@ -265,8 +265,8 @@ void Tuya::handle_command_(uint8_t command, uint8_t version, const uint8_t *buff
     case TuyaCommandType::WIFI_STATE:
       this->send_wifi_status_();
       break;
-    case TuyaCommandType::WIFI_RESET: //to handle this we should enable AP mode (or factory mode), instead we use pair mode
-    case TuyaCommandType::WIFI_PAIR:
+    case TuyaCommandType::WIFI_RESET:  // enable AP (factory) mode (not implemented yet), instead use pair
+    case TuyaCommandType::WIFI_PAIR:   // we'll remain UP for at most 2 minutes (maybe less with boot time)
       cut_cloud_mode_ = true;
       break;
     case TuyaCommandType::DATAPOINT_REPORT:
