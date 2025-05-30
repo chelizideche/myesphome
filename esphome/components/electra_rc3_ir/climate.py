69ee523f73254ef89495dc26c36545fbb827fb6e
Roy Goldshtein <Roy.Goldshtein@GMail.com>
2025-05-31 00:46:54 +0300

    Switch to new schema

--- esphome/components/electra_rc3_ir/climate.py
+++ esphome/components/electra_rc3_ir/climate.py
@@ -1,7 +1,5 @@
 import esphome.codegen as cg
 from esphome.components import climate_ir
-import esphome.config_validation as cv
-from esphome.const import CONF_ID
 
 AUTO_LOAD = ["climate_ir"]
 CODEOWNERS = ["@roygolds"]
@@ -9,13 +7,19 @@ CODEOWNERS = ["@roygolds"]
 electra_rc3_ir_ns = cg.esphome_ns.namespace("electra_rc3_ir")
 ElectraRC3IR = electra_rc3_ir_ns.class_("ElectraRC3IR", climate_ir.ClimateIR)
 
-CONFIG_SCHEMA = climate_ir.climate_ir_with_receiver_schema(electra_rc3_ir_ns).extend(
-    {
-        cv.GenerateID(): cv.declare_id(ElectraRC3IR),
-    }
-)
+CONFIG_SCHEMA = climate_ir.climate_ir_with_receiver_schema(ElectraRC3IR)
+
+# .extend(
+#     {
+#         cv.GenerateID(): cv.declare_id(ElectraRC3IR),
+#     }
+# )
 
 
 async def to_code(config):
-    var = cg.new_Pvariable(config[CONF_ID])
-    await climate_ir.register_climate_ir(var, config)
+    await climate_ir.new_climate_ir(config)
+
+
+# async def to_code(config):
+#     var = cg.new_Pvariable(config[CONF_ID])
+#     await climate_ir.register_climate_ir(var, config)
