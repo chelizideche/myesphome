489ff7d6f86293abe10181baf194ecd2c02558da
Anton Sergunov <setosha@gmail.com>
2025-06-13 17:50:08 +0600

    diff cleanup

--- esphome/components/esp32_can/esp32_can.cpp
+++ esphome/components/esp32_can/esp32_can.cpp
@@ -1,8 +1,8 @@
-#include "esp32_can.h"
-
 #ifdef USE_ESP32
-#include "driver/twai.h"
+#include "esp32_can.h"
 #include "esphome/core/log.h"
+
+#include <driver/twai.h>
 
 // WORKAROUND, because CAN_IO_UNUSED is just defined as (-1) in this version
 // of the framework which does not work with -fpermissive
