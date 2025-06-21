c32d09aa66a1a99e492877768f4793f8dec89f9a
madmat17 <matthias.bux@gmx.at>
2025-06-21 20:55:14 +0200

    Update __init__.py

--- esphome/components/hamulight/__init__.py
+++ esphome/components/hamulight/__init__.py
@@ -6,16 +6,17 @@ from esphome import pins
 HAMULIGHT_NAMESPACE = cg.esphome_ns.namespace('hamulight')
 HamulightComponent = HAMULIGHT_NAMESPACE.class_('HamulightComponent', cg.Component)
 
-COMMAND_SCANNER_SCHEMA = cv.Schema({
-    cv.Optional("enabled", default=True): cv.boolean,
-})
-
 CONFIG_SCHEMA = cv.Schema({
     cv.GenerateID(): cv.declare_id(HamulightComponent),                                               # unique instance ID
     cv.Required("rf_transmit_pin"): pins.gpio_output_pin_schema,                                      # required PIN for RF transmission
     cv.Optional("led_pin"): pins.gpio_output_pin_schema,                                              # optional PIN for feedback LED
     cv.Required("rf_address"): cv.hex_uint16_t,                                                       # required 2-Byte RF address (HEX)
-    cv.Optional("command_scanner"): COMMAND_SCANNER_SCHEMA,                                           # optional command scanner block
+    cv.Optional("command_scanner"): cv.Schema({                                                       # optional command scanner block
+        cv.Optional("enabled", default=True): cv.boolean,
+        cv.Required(CONF_CMDSCAN_START): cv.use_id(cg.NUMBER_ID),
+        cv.Required(CONF_CMDSCAN_END): cv.use_id(cg.NUMBER_ID),
+        cv.Required(CONF_CMDSCAN_PAUSE): cv.use_id(cg.NUMBER_ID),
+    }),
 }).extend(cv.COMPONENT_SCHEMA)
 
 # Function to generate the C++ code from the YAML configuration file
@@ -40,7 +41,14 @@ async def to_code(config):
     cg.add(var.set_rf_address(config["rf_address"]))
 
     # Command Scanner (optional)
-    if "command_scanner" in config and config["command_scanner"].get("enabled", True):
-        cg.add(var.set_command_scanner_enabled(True))
+    if "command_scanner" in config:
+        cs_cfg = config["command_scanner"]
+        cg.add(var.set_command_scanner_enabled(cs_cfg.get("enabled", True)))
+        start = await cg.get_variable(cs_cfg[CONF_CMDSCAN_START])
+        end = await cg.get_variable(cs_cfg[CONF_CMDSCAN_END])
+        pause = await cg.get_variable(cs_cfg[CONF_CMDSCAN_PAUSE])
+        cg.add(var.set_cmdscan_start_number(start))
+        cg.add(var.set_cmdscan_end_number(end))
+        cg.add(var.set_cmdscan_pause_number(pause))
     else:
         cg.add(var.set_command_scanner_enabled(False))
