843a500a9c04ad822462b6b202263da24a2d7bf2
madmat17 <matthias.bux@gmx.at>
2025-06-22 15:18:12 +0200

    Update hamulight_button.h

--- esphome/components/hamulight/hamulight_button.h
+++ esphome/components/hamulight/hamulight_button.h
@@ -15,6 +15,12 @@ namespace hamulight {
 class HamulightButton : public button::Button {
  public:
   void set_callback(std::function<void()> cb) { cb_ = std::move(cb); }
+  std::string get_object_id() const override {
+    // Replace spaces with underscores for valid object_id
+    std::string name = this->get_name();
+    std::replace(name.begin(), name.end(), ' ', '_');
+    return name;
+  }
  protected:
   void press_action() override {
     if (cb_)
