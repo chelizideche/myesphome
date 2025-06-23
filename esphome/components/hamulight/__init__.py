c12dbb641827688f16bb660630b685cb56346ce4
madmat17 <matthias.bux@gmx.at>
2025-06-23 10:51:44 +0200

    Update __init__.py

--- esphome/components/hamulight/__init__.py
+++ esphome/components/hamulight/__init__.py
@@ -44,6 +44,7 @@ CONFIG_SCHEMA = cv.Schema(
     }
 ).extend(cv.COMPONENT_SCHEMA)
 
+
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
