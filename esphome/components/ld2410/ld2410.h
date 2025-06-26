461e10ca3e9b773ee275c4d882beb1fa5ddf0009
Keith Burzinski <kbx81x@gmail.com>
2025-06-26 00:27:05 -0500

    inline

--- esphome/components/ld2410/ld2410.h
+++ esphome/components/ld2410/ld2410.h
@@ -133,7 +133,7 @@ enum PeriodicDataValue : uint8_t { HEAD = 0xAA, END = 0x55, CHECK = 0x00 };
 
 enum AckDataStructure : uint8_t { COMMAND = 6, COMMAND_STATUS = 7 };
 
-static int two_byte_to_int(char firstbyte, char secondbyte) { return (int16_t) (secondbyte << 8) + firstbyte; }
+static inline int two_byte_to_int(char firstbyte, char secondbyte) { return (int16_t) (secondbyte << 8) + firstbyte; }
 
 class LD2410Component : public Component, public uart::UARTDevice {
 #ifdef USE_SENSOR
