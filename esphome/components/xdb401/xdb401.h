3fd6144206106c59b702d40749581f3e5ad50d06
DanielV <daniel.vikstrom@gmail.com>
2025-07-02 07:45:55 +0200

    Update esphome/components/xdb401/xdb401.h
    Co-authored-by: Jesse Hills <3060199+jesserockz@users.noreply.github.com>

--- esphome/components/xdb401/xdb401.h
+++ esphome/components/xdb401/xdb401.h
@@ -13,7 +13,6 @@ class XDB401Component : public PollingComponent, public i2c::I2CDevice {
   void set_temperature_sensor(sensor::Sensor *temperature_sensor) { this->temperature_sensor_ = temperature_sensor; }
   void set_raw_pressure_sensor(sensor::Sensor *pressure_sensor) { this->pressure_sensor_ = pressure_sensor; }
 
-  float get_setup_priority() const override { return setup_priority::DATA; }
   void setup() override;
   void dump_config() override;
   void update() override;
